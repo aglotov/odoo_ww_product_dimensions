83f6399acf5dee669ea47be604acb6017e108073
Oleksandr Glotov <ag@wwind.ua>
2022-04-19 23:50:29 +0000

    Added area

--- models/product_product.py
+++ models/product_product.py
@@ -7,13 +7,24 @@ class ProductProduct(models.Model):
     ww_length = fields.Char(string='Length')
     ww_width = fields.Char(string='Width')
     ww_height = fields.Char(string='Height')
-    ww_dimensions_uom_name = fields.Char(string='Dimensions unit of measure label',
-                                         compute='_compute_ww_dimension_uom_name')
+    ww_area = fields.Char(string='Area')
+    ww_length_uom_name = fields.Char(string='Length unit of measure label',
+                                     compute='_compute_ww_length_uom_name')
+    ww_area_uom_name = fields.Char(string='Area unit of measure label',
+                                   compute='_compute_ww_area_uom_name')
 
     @api.model
-    def _get_dimensions_uom_name_from_ir_config_parameter(self):
-        uom_id = self.env['ir.config_parameter'].sudo().get_param('ww_product_dimensions.uom')
+    def _get_length_uom_name_from_ir_config_parameter(self):
+        uom_id = self.env['ir.config_parameter'].sudo().get_param('ww_product_dimensions.length_uom')
         return self.env['uom.uom'].browse(int(uom_id)).display_name
 
-    def _compute_ww_dimension_uom_name(self):
-        self.ww_dimensions_uom_name = self._get_dimensions_uom_name_from_ir_config_parameter()
+    @api.model
+    def _get_area_uom_name_from_ir_config_parameter(self):
+        uom_id = self.env['ir.config_parameter'].sudo().get_param('ww_product_dimensions.area_uom')
+        return self.env['uom.uom'].browse(int(uom_id)).display_name
+
+    def _compute_ww_length_uom_name(self):
+        self.ww_length_uom_name = self._get_length_uom_name_from_ir_config_parameter()
+
+    def _compute_ww_area_uom_name(self):
+        self.ww_area_uom_name = self._get_area_uom_name_from_ir_config_parameter()
--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -4,11 +4,19 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    ww_product_dimensions_uom_category = fields.Many2one('uom.category',
-                                                         string='Product dimensions UOM category',
-                                                         config_parameter='ww_product_dimensions.uom_category'
-                                                         )
-    ww_product_dimensions_uom = fields.Many2one('uom.uom',
-                                                string='Product dimensions UOM',
-                                                config_parameter='ww_product_dimensions.uom'
-                                                )
+    ww_product_length_uom_category = fields.Many2one('uom.category',
+                                                     string='Product length UoM category',
+                                                     config_parameter='ww_product_dimensions.length_uom_category'
+                                                     )
+    ww_product_length_uom = fields.Many2one('uom.uom',
+                                            string='Product length UoM',
+                                            config_parameter='ww_product_dimensions.length_uom',
+                                            )
+    ww_product_area_uom_category = fields.Many2one('uom.category',
+                                                   string='Product area UoM category',
+                                                   config_parameter='ww_product_dimensions.area_uom_category'
+                                                   )
+    ww_product_area_uom = fields.Many2one('uom.uom',
+                                          string='Product area UoM',
+                                          config_parameter='ww_product_dimensions.area_uom',
+                                          )
